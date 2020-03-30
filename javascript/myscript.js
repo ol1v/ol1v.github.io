54afa74aa7435abad33054d556c20e7ce057b9c8
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-03-30 14:08:18 +0200

    added javascriptfile to handle onclick events

---
+++ javascript/myscript.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var myBtn = document.getElementById("colab-btn");
+    myBtn.style.display = "none";
+
+    var myForm = document.getElementById("form-container")
+    myForm.style.display = "inline";
+    // document.getElementByClassName("form-container")[0]
+    console.log(myForm);
+}
